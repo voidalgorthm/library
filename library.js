9005753cced5886b4c82bc5128bab16f1271f9cf
voidalgorthm <3atlas.navis.noos@gmail.com>
2022-06-10 17:16:13 +0800

    feat: add add book function for adding books from user input

--- library.js
+++ library.js
@@ -47,39 +47,39 @@ function addBook() {
     hole.querySelector('button.add').addEventListener('click', () => {
       createForm();
       container.querySelector('div.overlay').addEventListener('click', (event) => {
-        if (event.target === container.querySelector('.overlay')) {
-          container.removeChild(container.querySelector('.overlay'));
-        }
-      }, true);
+        if (event.target === container.querySelector('.overlay')) { removeForm(); } }, true);
     });
   });
+}
+
+function removeForm() {
+  container.removeChild(container.querySelector('.overlay'));
 }
 
 function createForm() {
   const overlay = document.createElement('div');
   const prompt = document.createElement('div');
   const label = document.createElement('h2');
+
   label.appendChild(document.createTextNode('New Book:'));
   prompt.appendChild(label);
-
   const form = document.createElement('form');
-  setAttributes(form, { method: 'post', action: '', name: 'add_book', target: '_self' });
+  setAttributes(form, { method: 'post', id: 'Books' });
 
   const titleLabel = document.createElement('label');
   titleLabel.appendChild(document.createTextNode('Title:'));
   const title = document.createElement('input');
-  setAttributes(title, { type: 'text', id: 'title', name: 'book_title' });
+  setAttributes(title, { type: 'text', id: 'title', name: 'title' });
   titleLabel.appendChild(title);
-
   const authorLabel = document.createElement('label');
   authorLabel.appendChild(document.createTextNode('Author:'));
   const author = document.createElement('input');
-  setAttributes(author, { type: 'text', id: 'author', name: 'book_author' });
+  setAttributes(author, { type: 'text', id: 'author', name: 'author' });
   authorLabel.appendChild(author);
   const pagesLabel = document.createElement('label');
   pagesLabel.appendChild(document.createTextNode('Pages:'));
   const pages = document.createElement('input');
-  setAttributes(pages, { type: 'number', id: 'pages', name: 'book_pages' });
+  setAttributes(pages, { type: 'number', id: 'pages', name: 'pages' });
   pagesLabel.appendChild(pages);
   const readLabelSection = document.createElement('div');
   const readLabel = document.createElement('label');
@@ -88,7 +88,7 @@ function createForm() {
   const checkbox = document.createElement('div');
   checkbox.className = 'checkbox';
   const read = document.createElement('input');
-  setAttributes(read, { type: 'checkbox', id: 'read', name: 'book_read' });
+  setAttributes(read, { type: 'checkbox', id: 'read', name: 'read' });
   const readCheck = document.createElement('label');
   readCheck.className = 'readCheck';
   setAttributes(readCheck, { for: 'read' });
@@ -98,6 +98,7 @@ function createForm() {
   readLabelSection.appendChild(readLabel);
   readLabelSection.appendChild(checkbox);
   const submit = document.createElement('button');
+  setAttributes(submit, { type: 'submit', form: 'Books', formaction: 'post' });
   submit.textContent = 'Submit';
 
   form.appendChild(titleLabel);
@@ -105,26 +106,44 @@ function createForm() {
   form.appendChild(pagesLabel);
   form.appendChild(readLabelSection);
   form.appendChild(submit);
-  prompt.appendChild(form);
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+  });
+
+  submit.addEventListener('click', () => {
+    const rawBooks = Object.values(form).reduce((obj, field) => { obj[field.name] = field.value; return obj }, {});
+    let bookProperties = [];
+    Object.entries(rawBooks).forEach(([key, value]) => { if(key !== '') { bookProperties.push(value); } });
+    console.log(bookProperties);
+    
+    const newBook = new Book(...bookProperties);
+    library.push(newBook);
+    console.log(library);
+    removeForm();
+  });
 
   overlay.appendChild(prompt).className = 'prompt';
   container.appendChild(overlay).className = 'overlay';
+  prompt.appendChild(form);
+  title.focus();
 }
 
 let library = [];
 
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+class Book {
+  constructor(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.info = function () {
+      return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+    }
   }
 }
 
-const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-
-console.log(hobbit.info());
+const hobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'on');
+library.push(hobbit);
 
 function setAttributes(elem, attrs) { Object.entries(attrs).forEach(([key, value]) => elem.setAttribute(key, value)); }
